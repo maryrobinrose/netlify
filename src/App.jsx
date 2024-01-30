36a0b779de03873125e87026d100caa54218aca1
Mary <maryrobinrose@gmail.com>
2024-01-30 12:35:45 -0800

    add in exercise questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <h1>Netlify Exercise</h1>
+      <h2>Mary Robin Rose</h2>
+      <ol>
+        <li className="question">Talk about how you made your site and why you chose the tools you did. Briefly explain one challenge you experienced in setting up this site and how you overcame it.</li>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum urna vel nisl aliquet, eu dignissim justo pellentesque. Cras lacinia, quam eget fringilla tempus, metus urna tincidunt lectus, eget ultricies justo nisl in risus. Praesent vel convallis quam.
+          </p>
+        <li className="question">What did you think of our service during the time you used it? Provide some constructive criticism or some features that impressed you.</li>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum urna vel nisl aliquet, eu dignissim justo pellentesque. Cras lacinia, quam eget fringilla tempus, metus urna tincidunt lectus, eget ultricies justo nisl in risus. Praesent vel convallis quam.
+          </p>
+        <li className="question">Rank your 5 favorite and 5 least favorite activities from this list: https://gist.github.com/laurajodz/592402a6336410377dee1a744af846ab</li>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum urna vel nisl aliquet, eu dignissim justo pellentesque. Cras lacinia, quam eget fringilla tempus, metus urna tincidunt lectus, eget ultricies justo nisl in risus. Praesent vel convallis quam.
+          </p>
+        <li className="question">Provide a link to documentation for a technical/developer-focused product, which you think are well done, and explain why you think they are well done.</li>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum urna vel nisl aliquet, eu dignissim justo pellentesque. Cras lacinia, quam eget fringilla tempus, metus urna tincidunt lectus, eget ultricies justo nisl in risus. Praesent vel convallis quam.
+          </p>
+        <li className="question">Explain, in a couple of paragraphs, what you think are two major challenges around DNS configuration for less-technical customers hosting websites.</li>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum urna vel nisl aliquet, eu dignissim justo pellentesque. Cras lacinia, quam eget fringilla tempus, metus urna tincidunt lectus, eget ultricies justo nisl in risus. Praesent vel convallis quam.
+          </p>
+        <li className="question">A customer writes in to Support saying simply that their “site won’t build”. You have access to their build logs, and there you see this error: Build failed due to a user error: Build script returned non-zero exit code: 2. You have no more information than this and the site’s source repository is private so you cannot test the build yourself. How would you troubleshoot this issue? What steps would you take? Also, please compose your best customer-facing first response.</li>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum urna vel nisl aliquet, eu dignissim justo pellentesque. Cras lacinia, quam eget fringilla tempus, metus urna tincidunt lectus, eget ultricies justo nisl in risus. Praesent vel convallis quam.
+          </p>
+        <li className="question">How would you set up an http 301 status redirect from “/netlify/anything” on your site, to https://www.google.com/search?q=anything. Please provide the redirect formatting here. Now, how about a proxy redirect? Please add that proxy redirect rule directly to your site.</li>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum urna vel nisl aliquet, eu dignissim justo pellentesque. Cras lacinia, quam eget fringilla tempus, metus urna tincidunt lectus, eget ultricies justo nisl in risus. Praesent vel convallis quam.
+          </p>
+        <li className="question">Please attempt to deploy a function on our service. This need not be complicated. It could be "Hello World". Note that failure to deploy is not failing the exercise! Whether you have trouble or not, please describe what you experienced and how you attempted to troubleshoot any issues you encountered.</li>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum urna vel nisl aliquet, eu dignissim justo pellentesque. Cras lacinia, quam eget fringilla tempus, metus urna tincidunt lectus, eget ultricies justo nisl in risus. Praesent vel convallis quam.
+          </p>
+        <li className="question">We understand you don't know anything about our internal procedures at this stage, but we want you to explain at a high level how you'd react to this situation: You receive a report of a severe security issue on www.netlify.com. You can't immediately confirm the report, so what steps might you take to investigate or substantiate the report? What might you say to the reporter, even though we haven't confirmed their assertion yet, that will instill confidence that our business is very concerned about security? You believe there is a reasonable chance the report is correct and the problem is very large and impactful. How might you escalate?</li>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum urna vel nisl aliquet, eu dignissim justo pellentesque. Cras lacinia, quam eget fringilla tempus, metus urna tincidunt lectus, eget ultricies justo nisl in risus. Praesent vel convallis quam.
+          </p>
+      </ol>
+      <footer>
+        <p className="footer">
+          Check out the <a href="https://github.com/maryrobinrose/netlify" target="_blank">repo for this site here.</a>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </footer>
     </>
   )
 }
