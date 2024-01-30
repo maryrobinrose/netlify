05c5d64de2197621a6d8d37ea94c30e3dc0bc56c
Mary <maryrobinrose@gmail.com>
2024-01-30 15:55:25 -0800

    revert message

--- netlify/functions/newFunction.js
+++ netlify/functions/newFunction.js
@@ -1,6 +1,6 @@
 exports.handler = async function (event, context) {
     return {
       statusCode: 200,
-      body: JSON.stringify(Hello World!),
+      body: JSON.stringify({ message: "Hello World!" }),
     };
   };
